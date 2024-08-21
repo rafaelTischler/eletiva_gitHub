f911a6d09a4304b32c11e4c65587a9bfc7b45c1a
rafaelTischler <148276878+rafaelTischler@users.noreply.github.com>
2024-08-20 21:06:18 -0300

    commit aula 2

---
+++ site_gitAula2/principal.js
@@ -0,0 +1,8 @@
+var banners = ["Os melhores do Brasil", "Qualidade e preço baixo!"];
+var banner = 0;
+
+function trocaBanner() {
+  banner = (banner + 1) % 2;
+  document.querySelector("h2#mensagem").textContent = banners[banner];
+}
+setInterval(trocaBanner, 1000);
